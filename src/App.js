3c962dd4f72d95efb1849316329e8b08b49a66e5
RegAse <gudmundur210@gmail.com>
2022-10-09 17:28:42 +0000

    Implemented methods to retrieve current week days

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {useState, useEffect} from 'react';
+
+// ScheduleItem is like a todo item....
+
+function getFirstDayOfCurrentWeek(d)
+{
+  d = new Date();
+  let day = d.getDay();
+  let firstDay = d.getDate() - day + (day == 0 ? -6 : 1);
+  return new Date(d.setDate(firstDay));
+}
+
+// Returns the week of the day that is send
+function getWeekByDay(d)
+{
+  let firstDayOfWeek = getFirstDayOfCurrentWeek();
+
+  let daysOfWeek = [new Date(firstDayOfWeek)]
+  for (let i = 0; i < 6; i++) {
+    // firstDayOfWeek.setDate(firstDayOfWeek.getDate() + 1);
+
+    let newDay = new Date(firstDayOfWeek.setDate(firstDayOfWeek.getDate() + 1));
+    daysOfWeek.push(newDay);
+  }
+  return daysOfWeek;
+}
 
 function App() {
+  const [data, setData] = useState([
+    { Id: 0, title: "Yoga", dateFrom: "2022-10-09T08:30:00.00Z", dateTo:"2022-10-09T09:30:00.00Z" },
+    { Id: 1, title: "Wake Up", dateFrom: "2022-10-09T08:00:00.00Z", dateTo:"2022-10-09T08:30:00.00Z" },
+  ]);
+  
+  const [week, setWeek] = useState(getWeekByDay())
+
+  useEffect(() => {
+
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container-fluid'>
+        <div className='row'>
+        {
+        week.map((day) => (
+          <div className='col-sm-2' key={day}>
+            <div className='schedule-day'>
+              <h3>Day: {day.toLocaleString()}</h3>
+            </div>
+          </div>
+        ))
+        }
+        </div>
+      </div>
     </div>
   );
 }
